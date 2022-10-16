631fd7acf4c3ed536191189e55880b179dc6eb2e
Wone11 <gelayegeremew@gmail.com>
2022-10-16 23:08:48 +0300

    list file modified for changing appends

--- list.py
+++ list.py
@@ -1,11 +1,15 @@
-#lists 
-
-from operator import index
-
-
+#orginal list
 fruit_list= ['banana','apple','orange']
+
+#append an item by default position thet appends at the end
 fruit_list.append('lemon')
 print('Friut List: ',fruit_list)
+
+#insert an item in the specific position
+fruit_list.insert(3,'pappaya')
+
+#add an item in the first position
+fruit_list.insert(0,'tomato')
 
 #displaying lists using a for loop
 for (i, item) in enumerate(fruit_list,start=1):
