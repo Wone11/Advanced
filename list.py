78cc80b6b803f905a40b88a6be9202d6985c336c
Wone11 <gelayegeremew@gmail.com>
2022-10-16 23:00:40 +0300

    lemon fruit appended to the list

--- list.py
+++ list.py
@@ -3,9 +3,11 @@
 from operator import index
 
 
-friut_list= ['banana','apple','orange']
-print('Friut List: ',friut_list)
+fruit_list= ['banana','apple','orange']
+fruit_list.append('lemon')
+print('Friut List: ',fruit_list)
 
 #displaying lists using a for loop
-for (i, item) in enumerate(friut_list,start=1):
+for (i, item) in enumerate(fruit_list,start=1):
     print('index : {} fruit : {} '.format(i,item))
+
