bac2f548c6b35a526548d78cfa853881602223af
Wone11 <gelayegeremew@gmail.com>
2022-10-16 23:12:19 +0300

    carrot appended by master branch at position 1

--- list.py
+++ list.py
@@ -11,6 +11,9 @@ fruit_list.insert(3,'pappaya')
 #add an item in the first position
 fruit_list.insert(0,'tomato')
 
+#item inserted by master
+fruit_list.insert(1,'Carrot')
+
 #displaying lists using a for loop
 for (i, item) in enumerate(fruit_list,start=1):
     print('index : {} fruit : {} '.format(i,item))
