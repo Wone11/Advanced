339ff481207bf754579937b93750cb3394aa4ae6
Wone11 <gelayegeremew@gmail.com>
2022-10-18 12:55:22 +0300

    astrix function created and assembled

--- tuples.py
+++ tuples.py
@@ -1,10 +1,15 @@
 
-for item in range(0,10):
-    print(' * ' * item)
+'''
+Astrix function that displas a triangle of right angle and its reverse image
+'''
+def Astrix():
+    for item in range(0,10):
+        print(' * ' * item)
 
-for item in range(10,0,-1):
-    print(' * ' * item)
-
+    for item in range(10,0,-1):
+        print(' * ' * item)
+        
+'''Apple letters tuple'''
 def Letters():
     apple = ('a','p','p','l','e')
     print(apple)
@@ -15,3 +20,4 @@ def Letters():
         print('A does not exists in the tuple')
 
 Letters()
+Astrix()
